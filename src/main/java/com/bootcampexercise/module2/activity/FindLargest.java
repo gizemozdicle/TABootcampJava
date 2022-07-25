0e49dda0c9d000c603177a5bbd6090415e38d512
gizemozdicle <gizemozdicle@gmail.com>
2022-07-25 17:06:11 +0300

    Find largest

---
+++ src/main/java/com/bootcampexercise/module2/activity/FindLargest.java
@@ -0,0 +1,23 @@
+package com.bootcampexercise.module2.activity;
+
+public class FindLargest {
+
+    public static void main(String[] args) {
+
+		int x = 30;
+
+		int y = 20;
+
+
+		if (x > y) {
+			System.out.println(x + " is largest of two numbers");
+		}
+
+		else {
+			System.out.println(y + " is largest of two numbers");
+		}
+
+
+	}
+
+}
