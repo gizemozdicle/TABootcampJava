39e13d128aaf008388b347952ca7d996d1211117
gizemozdicle <gizemozdicle@gmail.com>
2022-07-26 09:26:27 +0300

    Add whole number

---
+++ src/main/java/com/bootcampexercise/module2/activity/AddWholeNum.java
@@ -0,0 +1,22 @@
+package com.bootcampexercise.module2.activity;
+
+public class AddWholeNum {
+
+
+
+    public static void main(String[] args) {
+
+        int sum=50;
+
+        for (int i = 50; i < 100; i++) {
+
+            sum=sum+1;
+
+            System.out.println("Sum of numbers from 50 to 100 is " +sum);
+
+        }
+
+
+    }
+
+}
