25de40d573a3ba1db57d5745563a298c252345df
gizemozdicle <gizemozdicle@gmail.com>
2022-07-26 14:21:26 +0300

    date activity

---
+++ src/main/java/com/bootcampexercise/module3/DateActivity.java
@@ -0,0 +1,12 @@
+package com.bootcampexercise.module3;
+import java.util.Date;
+
+public class DateActivity {
+
+
+    public static void main(String[] args) {
+        Date todayDate = new Date();
+        System.out.println(todayDate);
+    }
+
+}
--- src/main/java/com/bootcampexercise/module3/khshs.java
+++
@@ -1,4 +0,0 @@
-package com.bootcampexercise.module3;
-
-public class khshs {
-}
